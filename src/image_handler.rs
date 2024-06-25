b371c5c711cd908952a1d19729416bd12ce1a1f0
moeller-marc <122219863+moeller-marc@users.noreply.github.com>
2024-06-25 12:19:37 +0200

    fix bug in image_handler, minor refactoring in other files

--- src/image_handler.rs
+++ src/image_handler.rs
@@ -1,52 +1,75 @@
-    pub mod image_handler {
-        use std::io::Cursor;
-        use image::{io::Reader as ImageReader, GenericImageView};
+pub mod image_handler {
+    use core::panic;
+    use image::{io::Reader as ImageReader, GenericImageView};
+    use std::fs::File;
+    use std::{
+        fmt::format,
+        io::{Cursor, ErrorKind},
+    };
 
-        pub fn input_image(image_path: String) -> image::RgbImage {
-            let img = ImageReader::open(image_path).unwrap().decode().unwrap().to_rgb8();
+    pub fn input_image(image_path: String) -> image::RgbImage {
+        let img = ImageReader::open(image_path)
+            .unwrap()
+            .decode()
+            .unwrap()
+            .to_rgb8(); // TODO:remove unwpap statement
 
-            img
-        }
-
-        pub fn save_image(image: image::RgbImage, image_path: String) -> () {
-            image.save(image_path).unwrap();
-        }
-
-        pub fn new(width:i32, height:i32) -> image::RgbImage {
-            let img = image::RgbImage::new(width as u32, height as u32);
-            img
+        img
     }
 
-    }
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        use super::image_handler;
+    pub fn save_image(image: image::RgbImage, image_path: String) -> String {
+        // check and if nessesary create image
+        let file = match File::create_new(image_path.clone()) {
+            Ok(file) => file,
+            Err(error) => File::open(image_path.clone()).unwrap(), // TODO: handle this unwrap statement gracefully
+        };
 
-
-        #[test]
-        fn test_input_image() {
-            let image_path = String::from("test_images/cat_lying_blue_eye_small_ginger_fur_heal_pet_animal-609263.jpeg");
-            let img = image_handler::input_image(image_path);
-            assert_eq!(img.height(), 800);
-            assert_eq!(img.width(), 1200);
-        }
-
-        #[test]
-        fn test_save_image() {
-            let image_path = String::from("test_images/cat_lying_blue_eye_small_ginger_fur_heal_pet_animal-609263.jpeg");
-            let img = image_handler::input_image(image_path.clone());
-            let new_image_path = String::from("test_images/cat_lying_blue_eye_small_ginger_fur_heal_pet_animal-609263_copy.jpeg");
-            image_handler::save_image(img, new_image_path.clone());
-            let img = image_handler::input_image(new_image_path);
-            assert_eq!(img.height(), 800);
-            assert_eq!(img.width(), 1200);
-        } 
-
-        #[test]
-        fn test_create_image_file() {
-            let img = image_handler::new(800, 1200);
-            assert_eq!(img.width(), 800);
-            assert_eq!(img.height(), 1200);
+        // save image
+        match image.save(image_path) {
+            Ok(T) => String::from("succes"),
+            Err(err) => panic!("could not save file because of error:{:?}", err),
         }
     }
+
+    pub fn new(width: i32, height: i32) -> image::RgbImage {
+        let img = image::RgbImage::new(width as u32, height as u32);
+        img
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::image_handler;
+    use super::*;
+
+    #[test]
+    fn test_input_image() {
+        let image_path = String::from(
+            "test_images/cat_lying_blue_eye_small_ginger_fur_heal_pet_animal-609263.jpeg",
+        );
+        let img = image_handler::input_image(image_path);
+        assert_eq!(img.height(), 800);
+        assert_eq!(img.width(), 1200);
+    }
+
+    #[test]
+    fn test_save_image() {
+        let image_path = String::from(
+            "test_images/cat_lying_blue_eye_small_ginger_fur_heal_pet_animal-609263.jpeg",
+        );
+        let img = image_handler::input_image(image_path.clone());
+        let new_image_path = String::from(
+            "test_images/cat_lying_blue_eye_small_ginger_fur_heal_pet_animal-609263_copy.jpeg",
+        );
+        image_handler::save_image(img, new_image_path.clone());
+        let img = image_handler::input_image(new_image_path);
+        assert_eq!(img.height(), 800);
+        assert_eq!(img.width(), 1200);
+    }
+
+    #[test]
+    fn test_create_image_file() {
+        let img = image_handler::new(800, 1200);
+        assert_eq!(img.width(), 800);
+        assert_eq!(img.height(), 1200);
+    }
+}
--- src/vectors.rs
+++ src/vectors.rs
@@ -1,7 +1,7 @@
 pub mod vectors {
 
     pub trait Vector3 {
-        fn new(x:f32, y:f32, z:f32) -> Self;
+        fn new(x: f32, y: f32, z: f32) -> Self;
 
         fn x(&self) -> f32;
         fn y(&self) -> f32;
@@ -11,20 +11,19 @@ pub mod vectors {
         fn y_mut(&mut self) -> &mut f32;
         fn z_mut(&mut self) -> &mut f32;
 
-        
-        fn add<T:Vector3>(&mut self, other: T) -> () {
+        fn add<T: Vector3>(&mut self, other: T) -> () {
             *self.x_mut() += other.x();
             *self.y_mut() += other.y();
             *self.z_mut() += other.z();
         }
 
-        fn multiply_by_number(&mut self, num:f32) -> (){
+        fn multiply_by_number(&mut self, num: f32) -> () {
             *self.x_mut() *= num;
             *self.y_mut() *= num;
             *self.z_mut() *= num;
         }
 
-        fn divide_by_number(&mut self, num: f32) -> (){
+        fn divide_by_number(&mut self, num: f32) -> () {
             *self.x_mut() /= num;
             *self.y_mut() /= num;
             *self.z_mut() /= num;
@@ -39,28 +38,24 @@ pub mod vectors {
     }
 
     #[derive(Debug, PartialEq, Clone, Copy)]
-    pub struct Point3{
+    pub struct Point3 {
         x: f32,
         y: f32,
         z: f32,
     }
-    impl Vector3 for Point3{
-        fn new(x: f32, y:f32, z:f32) -> Self{
-            let point_out = Point3{
-                x: x,
-                y: y,
-                z: z,
-            };
+    impl Vector3 for Point3 {
+        fn new(x: f32, y: f32, z: f32) -> Self {
+            let point_out = Point3 { x: x, y: y, z: z };
             point_out
         }
 
-        fn x(&self) -> f32{
+        fn x(&self) -> f32 {
             self.x
         }
-        fn y(&self) -> f32{
+        fn y(&self) -> f32 {
             self.y
         }
-        fn z(&self) -> f32{
+        fn z(&self) -> f32 {
             self.z
         }
 
@@ -76,28 +71,24 @@ pub mod vectors {
     }
 
     #[derive(Debug, PartialEq, Clone, Copy)]
-    pub struct Color{
+    pub struct Color {
         r: f32,
         g: f32,
         b: f32,
     }
-    impl Vector3 for Color{
-        fn new(r: f32, g:f32, b:f32) -> Self{
-            let point_out = Color{
-                r: r,
-                g: g,
-                b: b,
-            };
+    impl Vector3 for Color {
+        fn new(r: f32, g: f32, b: f32) -> Self {
+            let point_out = Color { r: r, g: g, b: b };
             point_out
         }
 
-        fn x(&self) -> f32{
+        fn x(&self) -> f32 {
             self.r
         }
-        fn y(&self) -> f32{
+        fn y(&self) -> f32 {
             self.g
         }
-        fn z(&self) -> f32{
+        fn z(&self) -> f32 {
             self.b
         }
 
@@ -119,13 +110,27 @@ pub mod vectors {
         #[test]
         fn test_point3_new() {
             let p = Point3::new(1.0, 2.0, 3.0);
-            assert_eq!(p, Point3 { x: 1.0, y: 2.0, z: 3.0 });
+            assert_eq!(
+                p,
+                Point3 {
+                    x: 1.0,
+                    y: 2.0,
+                    z: 3.0
+                }
+            );
         }
 
         #[test]
         fn test_color_new() {
             let c = Color::new(0.1, 0.2, 0.3);
-            assert_eq!(c, Color { r: 0.1, g: 0.2, b: 0.3 });
+            assert_eq!(
+                c,
+                Color {
+                    r: 0.1,
+                    g: 0.2,
+                    b: 0.3
+                }
+            );
         }
 
         #[test]
@@ -133,7 +138,14 @@ pub mod vectors {
             let mut p1 = Point3::new(1.0, 2.0, 3.0);
             let p2 = Point3::new(4.0, 5.0, 6.0);
             p1.add(p2);
-            assert_eq!(p1, Point3 { x: 5.0, y: 7.0, z: 9.0 });
+            assert_eq!(
+                p1,
+                Point3 {
+                    x: 5.0,
+                    y: 7.0,
+                    z: 9.0
+                }
+            );
         }
 
         #[test]
@@ -144,35 +156,70 @@ pub mod vectors {
             c1.b = c1.b * 100.0;
             c1.b.round();
             c1.b = c1.b / 100.0;
-            assert_eq!(c1, Color { r: 0.5, g: 0.7, b: 0.9 });
+            assert_eq!(
+                c1,
+                Color {
+                    r: 0.5,
+                    g: 0.7,
+                    b: 0.9
+                }
+            );
         }
 
         #[test]
         fn test_point3_multiply_by_number() {
             let mut p = Point3::new(1.0, 2.0, 3.0);
             p.multiply_by_number(2.0);
-            assert_eq!(p, Point3 { x: 2.0, y: 4.0, z: 6.0 });
+            assert_eq!(
+                p,
+                Point3 {
+                    x: 2.0,
+                    y: 4.0,
+                    z: 6.0
+                }
+            );
         }
 
         #[test]
         fn test_color_multiply_by_number() {
             let mut c = Color::new(0.1, 0.2, 0.3);
             c.multiply_by_number(2.0);
-            assert_eq!(c, Color { r: 0.2, g: 0.4, b: 0.6 });
+            assert_eq!(
+                c,
+                Color {
+                    r: 0.2,
+                    g: 0.4,
+                    b: 0.6
+                }
+            );
         }
 
         #[test]
         fn test_point3_divide_by_number() {
             let mut p = Point3::new(2.0, 4.0, 6.0);
             p.divide_by_number(2.0);
-            assert_eq!(p, Point3 { x: 1.0, y: 2.0, z: 3.0 });
+            assert_eq!(
+                p,
+                Point3 {
+                    x: 1.0,
+                    y: 2.0,
+                    z: 3.0
+                }
+            );
         }
 
         #[test]
         fn test_color_divide_by_number() {
             let mut c = Color::new(0.2, 0.4, 0.6);
             c.divide_by_number(2.0);
-            assert_eq!(c, Color { r: 0.1, g: 0.2, b: 0.3 });
+            assert_eq!(
+                c,
+                Color {
+                    r: 0.1,
+                    g: 0.2,
+                    b: 0.3
+                }
+            );
         }
 
         #[test]
@@ -200,5 +247,3 @@ pub mod vectors {
         }
     }
 }
-
-
